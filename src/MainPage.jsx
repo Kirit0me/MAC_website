653f17ff1fbc1faa093883c2beedec456bdf91b1
Kirit0me <prabhavagarwal444@gmail.com>
2023-12-23 20:23:54 +0530

    i hate netlify

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from 'react'
-import Navbar from "./NavBar";
 
 function MainPage() {
     const [currentIndex, setCurrentIndex] = useState(0);
