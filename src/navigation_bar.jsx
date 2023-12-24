43ab93722d02aca54b7fc3af32c1799916cb681d
Kirit0me <prabhavagarwal444@gmail.com>
2023-12-24 23:18:08 +0530

    navvy wavvy baru p2

--- src/navigation_bar.jsx
+++ src/navigation_bar.jsx
@@ -1,27 +1,26 @@
 import { Link } from "react-router-dom"
 
 function Navigation_Bar() {
+    const navItems = [
+        { label: 'Home', path: '/' },
+        { label: 'Artwork', path: '/artwork' },
+        { label: 'Events', path: '/events' },
+        { label: 'Merch', path: '/merch' },
+        { label: 'History', path: '/history' },
+      ];
     return (
         <div class=" bg-slate-900 h-16 flex flex-row justify-between">
             <div class="p-2 "><img src="/mac.png" class="h-12 w-12"></img></div>
-            <div class="flex justify center space-x-4">
-                <div class="bg-slate-800 rounded-3xl text-pink-400">
-                    <div class="p-2 text-center font-bold pt-5"><Link to='/'>Home</Link></div>
-                </div>
-                <div class="bg-slate-800 rounded-3xl text-pink-400">
-                    <div class="p-2 text-center font-bold  pt-5"><Link to='/artwork'>Artwork</Link></div>
-                </div>
-                <div class="bg-slate-800 rounded-3xl text-pink-400">
-                    <div class="p-2 text-center font-bold pt-5"><Link to='/events'>Events</Link></div>
-                </div>
-                <div class="bg-slate-800 rounded-3xl text-pink-400">
-                    <div class="p-2 text-center font-bold  pt-5"><Link to='/merch'>Merch</Link></div>
-                </div>
-                <div class="bg-slate-800 rounded-3xl text-pink-400">
-                    <div class="p-2 text-center font-bold  pt-5"><Link to='/history'>History</Link></div>
-                </div>
+            <div className="flex justify-center space-x-4 pt-2">
+                {navItems.map((item, index) => (
+                    <div key={index} className="bg-slate-800 rounded-t-3xl text-pink-400">
+                        <div className="p-2 text-center pt-5 w-20">
+                            <Link to={item.path}>{item.label}</Link>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <span class="opacity-0"></span>
+            <span></span>
         </div>
     )
 }
