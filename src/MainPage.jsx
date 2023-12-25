b737b0a95d42ac13ebe2226d082dec77c4018a65
Kirit0me <prabhavagarwal444@gmail.com>
2023-12-25 11:57:52 +0530

    slideshow uwu

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,23 +1,48 @@
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import img1 from '/src/img/frieran.jpg'
-import img2 from '/src/img/spotlight.jpg'
+import img2 from '/src/img/aot.jpg'
 import img3 from '/src/img/379.jpg'
+import img4 from '/src/img/lucky_star_1.jpg'
+import img5 from '/src/img/higurashi.jpg'
+import img6 from '/src/img/holo.jpg'
+import img7 from '/src/img/hyouka.png'
+import img8 from '/src/img/miku1.jpg'
+import img9 from '/src/img/lucky_star_3.jpg'
+import img10 from '/src/img/yosuga.jpg'
+import img11 from '/src/img/fuxuan.jpg'
 
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+  
 function MainPage() {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const images = [img1, img2, img3]; 
+    let imgs = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+    const [shuffledImages, setShuffledImages] = useState(shuffle(imgs));
   
     useEffect(() => {
+      console.log('Component mounted or images updated');
+  
       const intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 2000); 
+        setCurrentIndex(prevIndex => {
+          console.log('Updating index', prevIndex, (prevIndex + 1) % shuffledImages.length);
+          return (prevIndex + 1) % shuffledImages.length;
+        });
+      }, 2000);
   
       return () => {
-        clearInterval(intervalId); 
+        console.log('Component unmounted');
+        clearInterval(intervalId);
       };
-    }, [images.length]);
-
+    }, [shuffledImages]);
     const navItems = [
         { label: 'About', path: '/about' },
         { label: 'Events', path: '/events' },
@@ -27,17 +52,17 @@ function MainPage() {
       ];
     return (
         <div class="relative h-screen">
-             {images.map((image, index) => (
-        <div
-            key={index}
-            className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ${
-                index === currentIndex ? 'opacity-20' : 'opacity-0'
-            }`}
-            style={{
-                backgroundImage: `url(${image})`,
-            }}
-            ></div>
-        ))}
+             {shuffledImages.map((image, index) => (
+                <div
+                    key={index}
+                    className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-1000 ${
+                        index === currentIndex ? 'opacity-20' : 'opacity-0'
+                    }`}
+                    style={{
+                        backgroundImage: `url(${image})`,
+                    }}
+                ></div>
+            ))}
             <div class="p-2"><img src="/mac.png" class="h-12 w-12"></img></div>
             <h1 class="text-7xl text-white text-center pt-20 ">
                 THE 
