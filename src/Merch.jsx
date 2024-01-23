f8a7a580c058206e18de59fd630a1d12de3823d3
Kirit0me <prabhavagarwal444@gmail.com>
2024-01-24 02:31:13 +0530

    slightly broken form

--- src/Merch.jsx
+++ src/Merch.jsx
@@ -33,7 +33,7 @@ function Merch() {
                             <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Omae no Namae</label>
                             <input 
                                 type="text" 
-                                name="name" 
+                                name="Name" 
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 placeholder="Name" 
                                 required />
