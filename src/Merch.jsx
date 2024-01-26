32e62cccaa7a59d7480bd3f4a7a9ad17c1f024b2
Kirit0me <prabhavagarwal444@gmail.com>
2024-01-26 12:57:06 +0530

    t shirts also added

--- src/Merch.jsx
+++ src/Merch.jsx
@@ -37,6 +37,39 @@ function Merch() {
             // Remove the anchor from the body
             document.body.removeChild(link);
           };
+
+          const handleDownloadStickers = () => {
+            const fileUrl = 'https://drive.google.com/file/d/1yf_E5pQ5DLgi2TOUXsk5CyNwIcoW-jmk/view?usp=drive_link';
+        
+            // Create a temporary anchor element
+            const link = document.createElement('a');
+            link.href = fileUrl;
+            link.target = '_blank'; // Open in a new tab
+            link.download = 'your-file-name.pdf'; // Specify the desired file name
+        
+            // Append the anchor to the body and trigger a click event
+            document.body.appendChild(link);
+            link.click();
+        
+            // Remove the anchor from the body
+            document.body.removeChild(link);
+          };
+          const handleDownloadTshirts = () => {
+            const fileUrl = 'https://drive.google.com/file/d/1bJfuQdq4pKW4FWZkifhXB_1uK1s_GBsj/view?usp=drive_link';
+        
+            // Create a temporary anchor element
+            const link = document.createElement('a');
+            link.href = fileUrl;
+            link.target = '_blank'; // Open in a new tab
+            link.download = 'your-file-name.pdf'; // Specify the desired file name
+        
+            // Append the anchor to the body and trigger a click event
+            document.body.appendChild(link);
+            link.click();
+        
+            // Remove the anchor from the body
+            document.body.removeChild(link);
+          };
       
 
     const [name, setName] = useState('');
@@ -47,12 +80,13 @@ function Merch() {
     const [poster, setPoster] = useState('');
     const [badge, setBadge] = useState('');
     const [pokeball, setPokeball] = useState('');
+    const [tshirt, setTshirt] = useState('');
 
 
     function handleSubmit(e){
 
         e.preventDefault();
-        console.log(name, regNo, mobile, stickers, custom, poster, badge, pokeball)
+        console.log(name, regNo, mobile, stickers, custom, poster, badge, pokeball, tshirt)
         console.log("Submitted")
         const data={
             Name: name,
@@ -62,7 +96,8 @@ function Merch() {
             Custom: custom,
             Poster: poster,
             Badge: badge,
-            Pokeball: pokeball
+            Pokeball: pokeball,
+            TShirt: tshirt
         }
         axios.post('https://sheet.best/api/sheets/896de3dd-75d0-4a60-9996-9ee245a654f8', data).then(response=>{
             console.log(response);
@@ -74,6 +109,7 @@ function Merch() {
             setPoster('');
             setBadge('');
             setPokeball('');
+            setTshirt('');
           })
       }
 
@@ -87,21 +123,20 @@ function Merch() {
                     <h1 className="font-bold text-2xl py-2">Badges : </h1>
                     <ul className="text-[15px]">
                         <li>High Quality Circular Images for customs</li>
-                        <li>Proper Distinct name eg: name_{name}_badge</li>
-                        <li>All PNGs sent to whatsapp number</li>
+                        <li>Proper Distinct name eg: {name}_img_name_badge</li>
                     </ul>
-                    <button onClick={handleDownloadBadges} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Download Default Badges</button>
+                    <button onClick={handleDownloadBadges} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Badges Catalogue</button>
                 </div>
                 <div className="text-white flex-col justify-center p-6 w-5/6 md:w-1/4 text-lg backdrop-blur-xl border-2 border-slate-500 rounded-3xl m-2">
                     <h1 className="font-bold text-2xl py-2">Posters : </h1>
                     <ul className="text-[15px]">
                         <li>High Quality Images, Upscaled Images, Only PDFs</li>
                         <li>No editing done by us</li>
-                        <li>Proper Distinct name eg: name_{name}_poster</li>
+                        <li>Proper Distinct name eg: {name}_img_name_poster</li>
                         <li>Size mentionaed as A4, A3</li>
                         <li>Dont combine the posters in one document for customs.</li>
                     </ul>
-                    <button onClick={handleDownloadPoster} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Download Default Posters</button>
+                    <button onClick={handleDownloadPoster} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Posters Catalogue</button>
                 </div>
                 <div className="text-white flex-col justify-center p-6 w-5/6 md:w-1/4 text-lg backdrop-blur-xl border-2 border-slate-500 rounded-3xl m-2">
                     <h1 className="font-bold text-2xl py-2">Stickers : </h1>
@@ -109,11 +144,25 @@ function Merch() {
                         <li>High Quality Transparent Images only for customs</li>
                         <li>Use recommended Sticker images for customs</li>
                         <li>Sizes Small(S), Medium(M), Large(L)</li>
-                        <li>All PNGs sent to whatsapp number</li>
                         <li>Dont combine the stickers in one document for customs.</li>
+                        <li>Proper Distinct name eg: {name}_img_name_stickers</li>
                     </ul>
+                    <button onClick={handleDownloadStickers} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Stickers Catalogue</button>
                 </div>
+                <div className="text-white flex-col justify-center p-6 w-5/6 md:w-1/4 text-lg backdrop-blur-xl border-2 border-slate-500 rounded-3xl m-2">
+                    <h1 className="font-bold text-2xl py-2">Stickers : </h1>
+                    <ul className="text-[15px]">
+                        <li>High Quality for customs</li>
+                        <li>No editing will be done</li>
+                        <li>Sizes S, M, L, XL, XXL</li>
+                        <li>Dont combine the Tshirts in one document for customs.</li>
+                    </ul>
+                    <button onClick={handleDownloadTshirts} className="mt-2 p-3 text-white border-2 border-white rounded-3xl hover:bg-stone-900">Tshirts Catalogue</button>
                 </div>
+                <div className="text-pink-300 flex-col justify-center p-6 w-5/6 md:w-1/4 text-lg backdrop-blur-xl border-2 border-slate-500 rounded-3xl m-2">
+                    <a href=" https://forms.gle/G1yyZ6sayFmk1wAC9">Use this <span className="text-blue-500">link</span> to submit your images:</a>
+                </div>
+            </div>
                 <div className="pt-10 flex justify-center text-xl">
                     <form id="formani" className="formanime w-2/3 md:w-1/2 m-6" onSubmit={handleSubmit}>
                         <div className="mb-5">
@@ -178,6 +227,16 @@ function Merch() {
                                 />
                         </div>
                         <div className="mb-5">
+                            <label for="tshirt" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Badges Details</label>
+                            <input 
+                                type="text" 
+                                onChange={(e) => setTshirt(e.target.value)}
+                                value={tshirt} 
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                placeholder="Serial Number_Size" 
+                                />
+                        </div>
+                        <div className="mb-5">
                             <label for="pokeball" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pokeball</label>
                             <input 
                                 type="text" 
@@ -194,7 +253,7 @@ function Merch() {
                                 onChange={(e) => setCustom(e.target.value)}
                                 value={custom}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                                placeholder="Name of File/s with Size" 
+                                placeholder="Comma seperated file names" 
                                  />
                         </div>
                         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
